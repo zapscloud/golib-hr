3aff741f7c77b3854f47adea15a87df5713fa375
Karthi <karthikeyan.n@inforios.com>
2023-10-06 17:55:54 +0530

    Update mongo_attendance_dao.go

--- hr_repository/mongodb_repository/mongo_attendance_dao.go
+++ hr_repository/mongodb_repository/mongo_attendance_dao.go
@@ -435,5 +435,47 @@ func (p *AttendanceMongoDBDao) appendListLookups(stages []bson.M) []bson.M {
 	// Add it to Aggregate Stage
 	stages = append(stages, lookupStage)
 
+	// Lookup Stage for Projects ========================================
+	lookupStage = bson.M{
+		hr_common.MONGODB_LOOKUP: bson.M{
+			hr_common.MONGODB_STR_FROM:         hr_common.DbHrProjects,
+			hr_common.MONGODB_STR_LOCALFIELD:   hr_common.FLD_CLOCK_IN + "." + hr_common.FLD_PROJECT_ID,
+			hr_common.MONGODB_STR_FOREIGNFIELD: hr_common.FLD_PROJECT_ID,
+			hr_common.MONGODB_STR_AS:           hr_common.FLD_PROJECT_INFO,
+			hr_common.MONGODB_STR_PIPELINE: []bson.M{
+				// Remove following fields from result-set
+				{hr_common.MONGODB_PROJECT: bson.M{
+					db_common.FLD_DEFAULT_ID:  0,
+					db_common.FLD_IS_DELETED:  0,
+					db_common.FLD_CREATED_AT:  0,
+					hr_common.FLD_BUSINESS_ID: 0,
+					db_common.FLD_UPDATED_AT:  0}},
+			},
+		},
+	}
+	// Add it to Aggregate Stage
+	stages = append(stages, lookupStage)
+
+	// Lookup Stage for Clients ========================================
+	lookupStage = bson.M{
+		hr_common.MONGODB_LOOKUP: bson.M{
+			hr_common.MONGODB_STR_FROM:         hr_common.DbHrClients,
+			hr_common.MONGODB_STR_LOCALFIELD:   hr_common.FLD_CLOCK_IN + "." + hr_common.FLD_CLIENT_ID,
+			hr_common.MONGODB_STR_FOREIGNFIELD: hr_common.FLD_CLIENT_ID,
+			hr_common.MONGODB_STR_AS:           hr_common.FLD_CLIENT_INFO,
+			hr_common.MONGODB_STR_PIPELINE: []bson.M{
+				// Remove following fields from result-set
+				{hr_common.MONGODB_PROJECT: bson.M{
+					db_common.FLD_DEFAULT_ID:  0,
+					db_common.FLD_IS_DELETED:  0,
+					db_common.FLD_CREATED_AT:  0,
+					hr_common.FLD_BUSINESS_ID: 0,
+					db_common.FLD_UPDATED_AT:  0}},
+			},
+		},
+	}
+	// Add it to Aggregate Stage
+	stages = append(stages, lookupStage)
+
 	return stages
 }
