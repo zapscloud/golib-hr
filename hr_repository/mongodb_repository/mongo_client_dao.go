6eaf7f6c3ccb958dce8347f0d25e53081e7655f2
anbu <anburajanvlw2@gmail.com>
2023-10-31 12:24:55 +0530

    changes

--- hr_repository/mongodb_repository/mongo_client_dao.go
+++ hr_repository/mongodb_repository/mongo_client_dao.go
@@ -30,7 +30,7 @@ func (p *ClientMongoDBDao) InitializeDao(client utils.Map, businessId string) {
 // List - List all Collections
 func (p *ClientMongoDBDao) List(filter string, sort string, skip int64, limit int64) (utils.Map, error) {
 	var results []utils.Map
-
+	var bFilter bool = false
 	log.Println("Begin - Find All Collection Dao", hr_common.DbHrClients)
 
 	collection, ctx, err := mongo_utils.GetMongoDbCollection(p.client, hr_common.DbHrClients)
@@ -40,17 +40,37 @@ func (p *ClientMongoDBDao) List(filter string, sort string, skip int64, limit in
 
 	log.Println("Get Collection - Find All Collection Dao", filter, len(filter), sort, len(sort))
 
-	opts := options.Find()
+	log.Println("Get Collection - Find All Collection Dao", filter, len(filter), sort, len(sort))
 
 	filterdoc := bson.D{}
 	if len(filter) > 0 {
 		// filters, _ := strconv.Unquote(string(filter))
+		// 20230803 Karthi: The second parameter should be false to interpret "$date" in JSON
 		err = bson.UnmarshalExtJSON([]byte(filter), true, &filterdoc)
 		if err != nil {
 			log.Println("Unmarshal Ext JSON error", err)
-			log.Println(filterdoc)
 		}
+		bFilter = true
 	}
+
+	// All Stages
+	stages := []bson.M{}
+
+	// Remove unwanted fields
+	unsetStage := bson.M{hr_common.MONGODB_UNSET: db_common.FLD_DEFAULT_ID}
+	stages = append(stages, unsetStage)
+
+	// Add Lookup stages
+	stages = p.appendListLookups(stages)
+
+	// Match Stage
+	filterdoc = append(filterdoc,
+		bson.E{Key: hr_common.FLD_BUSINESS_ID, Value: p.businessID},
+		bson.E{Key: db_common.FLD_IS_DELETED, Value: false})
+
+	filterdoc = append(filterdoc, bson.E{Key: db_common.FLD_IS_DELETED, Value: false})
+	matchStage := bson.M{hr_common.MONGODB_MATCH: filterdoc}
+	stages = append(stages, matchStage)
 
 	if len(sort) > 0 {
 		var sortdoc interface{}
@@ -58,26 +78,60 @@ func (p *ClientMongoDBDao) List(filter string, sort string, skip int64, limit in
 		if err != nil {
 			log.Println("Sort Unmarshal Error ", sort)
 		} else {
-			opts.SetSort(sortdoc)
+			sortStage := bson.M{hr_common.MONGODB_SORT: sortdoc}
+			stages = append(stages, sortStage)
+		}
+	}
+
+	var filtercount int64 = 0
+	if bFilter {
+		// Prepare Filter Stages
+		filterStages := stages
+
+		// Add Count aggregate
+		countStage := bson.M{hr_common.MONGODB_COUNT: hr_common.FLD_FILTERED_COUNT}
+		filterStages = append(filterStages, countStage)
+
+		//log.Println("Aggregate for Count ====>", filterStages, stages)
+
+		// Execute aggregate to find the count of filtered_size
+		cursor, err := collection.Aggregate(ctx, filterStages)
+		if err != nil {
+			log.Println("Error in Aggregate", err)
+			return nil, err
+		}
+		var countResult []utils.Map
+		if err = cursor.All(ctx, &countResult); err != nil {
+			log.Println("Error in cursor.all", err)
+			return nil, err
+		}
+
+		//log.Println("Count Filter ===>", countResult)
+		if len(countResult) > 0 {
+			if dataVal, dataOk := countResult[0][hr_common.FLD_FILTERED_COUNT]; dataOk {
+				filtercount = int64(dataVal.(int32))
+			}
+		}
+		// log.Println("Count ===>", filtercount)
+
+	} else {
+		filtercount, err = collection.CountDocuments(ctx, filterdoc)
+		if err != nil {
+			return nil, err
 		}
 	}
 
 	if skip > 0 {
-		log.Println(filterdoc)
-		opts.SetSkip(skip)
+		skipStage := bson.M{hr_common.MONGODB_SKIP: skip}
+		stages = append(stages, skipStage)
 	}
 
 	if limit > 0 {
-		log.Println(filterdoc)
-		opts.SetLimit(limit)
+		limitStage := bson.M{hr_common.MONGODB_LIMIT: limit}
+		stages = append(stages, limitStage)
 	}
 
-	filterdoc = append(filterdoc,
-		bson.E{Key: hr_common.FLD_BUSINESS_ID, Value: p.businessID},
-		bson.E{Key: db_common.FLD_IS_DELETED, Value: false})
-
-	log.Println("Parameter values ", filterdoc, opts)
-	cursor, err := collection.Find(ctx, filterdoc, opts)
+	cursor, err := collection.Aggregate(ctx, stages)
 	if err != nil {
 		return nil, err
 	}
@@ -88,42 +142,29 @@ func (p *ClientMongoDBDao) List(filter string, sort string, skip int64, limit in
 		return nil, err
 	}
 
-	//log.Println("End - Find All Collection Dao", results)
-
-	listdata := []utils.Map{}
-	for _, value := range results {
-		//log.Println("Item ", idx)
-		value = db_common.AmendFldsForGet(value)
-		listdata = append(listdata, value)
-	}
-
-	//log.Println("End - Find All Collection Dao", listdata)
-
-	log.Println("Parameter values ", filterdoc)
-	filtercount, err := collection.CountDocuments(ctx, filterdoc)
-	if err != nil {
-		return nil, err
-	}
-
 	basefilterdoc := bson.D{
 		{Key: hr_common.FLD_BUSINESS_ID, Value: p.businessID},
 		{Key: db_common.FLD_IS_DELETED, Value: false}}
+
 	totalcount, err := collection.CountDocuments(ctx, basefilterdoc)
 	if err != nil {
-		return nil, err
+		return utils.Map{}, err
+	}
+
+	if results == nil {
+		results = []utils.Map{}
 	}
 
 	response := utils.Map{
 		db_common.LIST_SUMMARY: utils.Map{
 			db_common.LIST_TOTALSIZE:    totalcount,
 			db_common.LIST_FILTEREDSIZE: filtercount,
-			db_common.LIST_RESULTSIZE:   len(listdata),
+			db_common.LIST_RESULTSIZE:   len(results),
 		},
-		db_common.LIST_RESULT: listdata,
+		db_common.LIST_RESULT: results,
 	}
 
 	return response, nil
-
 }
 
 // Get - Get account details
@@ -134,29 +175,50 @@ func (p *ClientMongoDBDao) Get(client_id string) (utils.Map, error) {
 	log.Println("accountMongoDao::Get:: Begin ", client_id)
 
 	collection, ctx, err := mongo_utils.GetMongoDbCollection(p.client, hr_common.DbHrClients)
+	if err != nil {
+		return nil, err
+	}
 	log.Println("Find:: Got Collection ")
 
+	log.Println("Find:: Got Collection ")
+	stages := []bson.M{}
 	filter := bson.D{
 		{Key: hr_common.FLD_CLIENT_ID, Value: client_id},
 		{Key: hr_common.FLD_BUSINESS_ID, Value: p.businessID},
 		{Key: db_common.FLD_IS_DELETED, Value: false}}
-
 	log.Println("Get:: Got filter ", filter)
 
-	singleResult := collection.FindOne(ctx, filter)
-	if singleResult.Err() != nil {
-		log.Println("Get:: Record not found ", singleResult.Err())
-		return result, singleResult.Err()
-	}
-	singleResult.Decode(&result)
+	matchStage := bson.M{"$match": filter}
+	stages = append(stages, matchStage)
+
+	// Add Lookup stages
+	stages = p.appendListLookups(stages)
+
+	log.Println("GetDetails:: Got filter ", filter)
+
+	// Aggregate the stages
+	singleResult, err := collection.Aggregate(ctx, stages)
 	if err != nil {
+		log.Println("GetDetails:: Error in aggregation: ", err)
+		return result, err
+	}
+
+	if !singleResult.Next(ctx) {
+		// No matching document found
+		err := &utils.AppError{ErrorCode: "S30102", ErrorMsg: "Record Not Found", ErrorDetail: "Given TxnID is not found"}
+		log.Println("GetDetails:: Record not found")
+		return result, err
+	}
+
+	if err := singleResult.Decode(&result); err != nil {
 		log.Println("Error in decode", err)
 		return result, err
 	}
+
 	// Remove fields from result
 	result = db_common.AmendFldsForGet(result)
 
-	log.Println("accountMongoDao::Get:: End Found a single document: \n", err)
+	log.Printf("TxnMongoDBDao::GetDetails:: End Found a single document: %+v\n", result)
 	return result, nil
 }
 
@@ -300,4 +362,48 @@ func (p *ClientMongoDBDao) DeleteAll() (int64, error) {
 	}
 	log.Printf("accountMongoDao::DeleteAll - End deleted %v documents\n", res.DeletedCount)
 	return res.DeletedCount, nil
+}
+func (p *ClientMongoDBDao) appendListLookups(stages []bson.M) []bson.M {
+
+	// Lookup Stage for PlatformAppUsers ========================================
+	lookupStage := bson.M{
+		hr_common.MONGODB_LOOKUP: bson.M{
+			hr_common.MONGODB_STR_FROM:         hr_common.DbHrShiftProfiles,
+			hr_common.MONGODB_STR_LOCALFIELD:   hr_common.FLD_SHIFT_PROFILE_ID,
+			hr_common.MONGODB_STR_FOREIGNFIELD: hr_common.FLD_SHIFT_PROFILE_ID,
+			hr_common.MONGODB_STR_AS:           hr_common.FLD_SHIFT_INFO,
+			hr_common.MONGODB_STR_PIPELINE: []bson.M{
+				// Remove following fields from result-set
+				{hr_common.MONGODB_PROJECT: bson.M{
+					db_common.FLD_DEFAULT_ID: 0,
+					db_common.FLD_IS_DELETED: 0,
+					db_common.FLD_CREATED_AT: 0,
+					db_common.FLD_UPDATED_AT: 0}},
+			},
+		},
+	}
+	// Add it to Aggregate Stage
+	stages = append(stages, lookupStage)
+
+	// Lookup Stage for User ==========================================
+	lookupStage = bson.M{
+		hr_common.MONGODB_LOOKUP: bson.M{
+			hr_common.MONGODB_STR_FROM:         hr_common.DbHrOvertimes,
+			hr_common.MONGODB_STR_LOCALFIELD:   hr_common.FLD_OVERTIME_ID,
+			hr_common.MONGODB_STR_FOREIGNFIELD: hr_common.FLD_OVERTIME_ID,
+			hr_common.MONGODB_STR_AS:           hr_common.FLD_OVERTIME_INFO,
+			hr_common.MONGODB_STR_PIPELINE: []bson.M{
+				// Remove following fields from result-set
+				{hr_common.MONGODB_PROJECT: bson.M{
+					db_common.FLD_DEFAULT_ID: 0,
+					db_common.FLD_IS_DELETED: 0,
+					db_common.FLD_CREATED_AT: 0,
+					db_common.FLD_UPDATED_AT: 0}},
+			},
+		},
+	}
+	// Add it to Aggregate Stage
+	stages = append(stages, lookupStage)
+
+	return stages
 }
